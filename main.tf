9e5709add7ad77d09e035c6dd26bbce06642dbda
hrmsk66 <hrmsk66@gmail.com>
2021-10-16 13:02:25 +0900

    Fix vars in main.tf

--- main.tf
+++ main.tf
@@ -60,8 +60,8 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  bucket_public_acls      = true
-  bucket_public_policy    = true
+  block_public_acls      = true
+  block_public_policy    = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
